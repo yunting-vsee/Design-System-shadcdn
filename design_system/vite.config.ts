66b09c6344d7062dcf3ae2df8ba8f73e7e9971d1
Yun-Ting Lien <yunting@vsee.com>
2026-03-29 18:16:48 -0400

    localhost address for VS Code

--- design_system/vite.config.ts
+++ design_system/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  server: {
+    port: 62708,
+    },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
